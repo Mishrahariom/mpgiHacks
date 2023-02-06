a2f41a3353e0e98b59d7189b2eb3a6a8728de688
Mishrahariom <mishrahariom2708@gmail.com>
2023-02-07 01:41:45 +0530

    adding navbar and footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Cover, About, Schedule, Prize, Sponsor, Faq, Contact} from './sections'
+import NavbarComponent from './component/NavbarComponent';
 
 function App() {
   return (
     <>
+    <NavbarComponent/>
         <Cover />
         <About />
         <Schedule />
