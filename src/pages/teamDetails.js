59ee526ad9f72524d98251582fef6a97e16c17a4
Mishrahariom <mishrahariom2708@gmail.com>
2023-02-10 21:15:46 +0530

    team section

--- src/pages/teamDetails.js
+++ src/pages/teamDetails.js
@@ -1,128 +1,193 @@
-export const head = [
+
+import kartik from '../assests/image/kartiksrivastava.jpg'
+// import kartik from '../assests'
+// import kartik from '../assests'
+// import kartik from '../assests'
+
+export const faculty = [
     {
-        name: "Adarsh Singh chauhan",
-        post: "President"
+        name: "Saurabh Singh",
+        photo: "",
+        post: "Head Of Department",
     },
     {
-        name: "Kalpna Shukla",
-        post: "Vice President"
-    },
-    {
-        name: "Arshveer Singh",
-        post: "Photography Head"
-    },
-    {
-        name: "Saksham Sahu",
-        post: "Events Head"
-    },
-    {
-        name: "Aanshi Mishra",
-        post: "Finance Head"
-    },
-    {
-        name: "Akancha Singh Kanoujia",
-        post: "Hospitality Head"
-    },
-    {
-        name: "Mukul dubey",
-        post: "Web development Head"
-    },
+        name: "Milind Bhatt",
+        post: "Assisment Professor",
+    }
 ]
 
-export const financeHeads = [
+export const founders = [
     {
-        name: "Virat mishra",
-        post: "Associate Finance Head"
+        name: "Hari Om Mishra",
+        post: "",
     },
     {
-        name: "Ayush Mishra",
-        post: "Associate Finance Head"
+        name: "Mukul Dubey ",
+        post: "",
+
     },
     {
-        name: "Sonam rajput",
-        post: "Associate Finance Head"
+        name: "Anmol pandey ",
+        post: " ",
+
     },
     {
-        name: "Tanya shivhare",
-        post: "Associate Finance Head"
+        name: "Anma Jha ",
+        post: " ",
+
     },
+    {
+        name: "Kartik Srivastava ",
+        post: "  ",
+
+    },
+    {
+        name: " Mohammad Sarfaraz",
+        post: "  ",
+        photo: "Screenshot 2023-02-10 155813.png"
+    },
+    {
+        name: " Arshveer Singh Arora ",
+        post: "     ",
+
+    },
+    {
+        name: "Risabh Vaish",
+        post: " ",
+
+    },
+    {
+        name: " Ashutosh Singh ",
+        post: " ",
+
+    }
 ]
 
-export const photographyHeads = [
-    {
-        name: "Shobhit Vishwakarma",
-        post: "Associate Photography Head"
-    },
-    {
-        name: "Shreya Trivedi",
-        post: "Associate Photography Head"
-    },
-    {
-        name: "Jatin Rastogi",
-        post: "Associate Photography Head"
-    },
-]
+// export const head = [
+//     {
+//         name: "Adarsh Singh chauhan",
+//         post: "President"
+//     },
+//     {
+//         name: "Kalpna Shukla",
+//         post: "Vice President"
+//     },
+//     {
+//         name: "Arshveer Singh",
+//         post: "Photography Head"
+//     },
+//     {
+//         name: "Saksham Sahu",
+//         post: "Events Head"
+//     },
+//     {
+//         name: "Aanshi Mishra",
+//         post: "Finance Head"
+//     },
+//     {
+//         name: "Akancha Singh Kanoujia",
+//         post: "Hospitality Head"
+//     },
+//     {
+//         name: "Mukul dubey",
+//         post: "Web development Head"
+//     },
+// ]
 
-export const hospitalityHeads = [
-    {
-        name: "Vikas Kumar Mourya",
-        post: "Associate Hospitality Head"
-    },
-    {
-        name: "Mohammad Sarfaraz",
-        post: "Associate Hopitality Head"
-    },
-    {
-        name: "Ashutosh Singh",
-        post: "Associate Hospitality Head"
-    },
-]
+// export const financeHeads = [
+//     {
+//         name: "Virat mishra",
+//         post: "Associate Finance Head"
+//     },
+//     {
+//         name: "Ayush Mishra",
+//         post: "Associate Finance Head"
+//     },
+//     {
+//         name: "Sonam rajput",
+//         post: "Associate Finance Head"
+//     },
+//     {
+//         name: "Tanya shivhare",
+//         post: "Associate Finance Head"
+//     },
+// ]
 
-export const eventHeads = [
-    {
-        name: "Vedansh Singh",
-        post: "Associate Event Head"
-    },
-    {
-        name: "Himani Singh",
-        post: "Associate Event Head"
-    },
-    {
-        name: "Saurabh Prajapati",
-        post: "Associate Event Head"
-    },
-    {
-        name: "Ayush Sharma",
-        post: "Associate Event Head"
-    },
-]
+// export const photographyHeads = [
+//     {
+//         name: "Shobhit Vishwakarma",
+//         post: "Associate Photography Head"
+//     },
+//     {
+//         name: "Shreya Trivedi",
+//         post: "Associate Photography Head"
+//     },
+//     {
+//         name: "Jatin Rastogi",
+//         post: "Associate Photography Head"
+//     },
+// ]
 
-export const techHead = [
-    {
-        name: "Aryan Singh",
-        post: "Associate Tech Head"
-    },
-    {
-        name: "Arya Gupta",
-        post: "Associate Tech Head"
-    },
-    {
-        name: "Ritik Saxena",
-        post: "Associate Tech Head"
-    },
-]
+// export const hospitalityHeads = [
+//     {
+//         name: "Vikas Kumar Mourya",
+//         post: "Associate Hospitality Head"
+//     },
+//     {
+//         name: "Mohammad Sarfaraz",
+//         post: "Associate Hopitality Head"
+//     },
+//     {
+//         name: "Ashutosh Singh",
+//         post: "Associate Hospitality Head"
+//     },
+// ]
 
-export const volunteer = [
-    {
-        name: "Kashif Akhtar",
-        post: "Volunteer"
-    },
-    {
-        name: "Shikhar Verma",
-        post: "Volunteer"
-    },
-    {
-        name: "Sneha Trivedi",
-        post: "Volunteer"
-    },
-]
+// export const eventHeads = [
+//     {
+//         name: "Vedansh Singh",
+//         post: "Associate Event Head"
+//     },
+//     {
+//         name: "Himani Singh",
+//         post: "Associate Event Head"
+//     },
+//     {
+//         name: "Saurabh Prajapati",
+//         post: "Associate Event Head"
+//     },
+//     {
+//         name: "Ayush Sharma",
+//         post: "Associate Event Head"
+//     },
+// ]
+
+// export const techHead = [
+//     {
+//         name: "Aryan Singh",
+//         post: "Associate Tech Head"
+//     },
+//     {
+//         name: "Arya Gupta",
+//         post: "Associate Tech Head"
+//     },
+//     {
+//         name: "Ritik Saxena",
+//         post: "Associate Tech Head"
+//     },
+// ]
+
+// export const volunteer = [
+//     {
+//         name: "Kashif Akhtar",
+//         post: "Volunteer"
+//     },
+//     {
+//         name: "Shikhar Verma",
+//         post: "Volunteer"
+//     },
+//     {
+//         name: "Sneha Trivedi",
+//         post: "Volunteer"
+//     },
+// ]
