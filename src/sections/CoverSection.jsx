ea31f77cb863a664670042fbd06ee4eea230a136
Mishrahariom <mishrahariom2708@gmail.com>
2023-02-10 21:27:51 +0530

    workoncard

--- src/sections/CoverSection.jsx
+++ src/sections/CoverSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 const coverSection = () => {
   return (
     <section className='panel cover vh-100 d-flex flex-column align-items-center justify-content-center' style={{"paddingTop": "5rem", "color": "gold", "background":"linear-gradient(#453402, #000000)"}}>
         <h3 style={{"fontSize": "50px",}}>Tech-e-Clan</h3>
         <p style={{ "color": "white"}}>presents</p>
-        <img src='./mpgiHacksPNG.png' style={{"width": "30%"}} />
+         
+        { <img src='./mpgiHacksPNG.png' style={{"width": "30%"}} /> }
         <h1>MPGI HACKSS 2023</h1>
     </section>
   )
