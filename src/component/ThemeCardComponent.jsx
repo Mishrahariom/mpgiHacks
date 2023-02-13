1e2c0236d0d8f41b150aa5c9bb9ca3ed5cc5ef50
Mishrahariom <hariom.cs2020@mpgi.edu.in>
2023-02-13 13:09:56 +0530

    added images in theme

--- src/component/ThemeCardComponent.jsx
+++ src/component/ThemeCardComponent.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const ThemeCardComponent = ({theme}) => {
+const ThemeCardComponent = ({themeName,themePhoto}) => {
   return (
     <>
-        <div className='card bg-dark m-3' style={{"width": "18rem", "height": "15rem"}}>
+        <div className='card bg-dark m-3' style={{"width": "18rem", "height": "15rem" , backgroundImage:`url(${themePhoto})` 
+        , backgroundPosition: "center",
+    backgroundSize: "cover"}}>
             <div className='card-body'>
-                <h5 className='card-title' style={{"color": "gold"}}>{theme}</h5>
+                <h5 className='card-title' style={{"color": "gold"}}>{themeName}</h5>
             </div>
         </div>
     </>
