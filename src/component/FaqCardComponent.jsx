4c1cdd7e592286cdc3d270e217de6d33812596e2
mukul1312 <mdubey.0433@gmail.com>
2023-03-29 20:28:01 +0530

    faq design changed

--- src/component/FaqCardComponent.jsx
+++ src/component/FaqCardComponent.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FaqCardComponent = ({question, answer, id}) => {
   return (
-    <div className="accordion-item bg-dark">
+    <div className="accordion-item " style={{"background": "transparent", "borderLeft": "0", "borderRight": "0", "borderTop": "0", "borderRadius": "0"}}>
       <h2 className="accordion-header" id={"heading" + id}>
         <button className="accordion-button bg-transparent text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + id} aria-expanded="true" aria-controls={"collapse" + id}>
-          <p className='text-warning'>{question}</p>
+          <p className='text-white'>{question}</p>
         </button>
       </h2>
       <div id={"collapse" + id} className="accordion-collapse collapse" aria-labelledby={"headingOne" + id} data-bs-parent="#accordionExample">
