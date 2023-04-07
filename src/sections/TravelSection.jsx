1969590415ab7f5d816db3596c1462e381a30604
Mishrahariom <hariom.cs2020@mpgi.edu.in>
2023-04-07 19:17:29 +0530

    all changes are done , speaker and

--- src/sections/TravelSection.jsx
+++ src/sections/TravelSection.jsx
@@ -14,23 +14,20 @@ const Travel = () => {
     // </div>
     // </div>
     // </div>
-    <div className='col-12 col-md-12 d-flex flex-column align-items-center jusitfy-content-center'>
-          
-    <h1 >Free Travel Expence</h1>
-    <p style={{ "fontSize":"50px" , "color": "gold", "text-align": "center" }}> Terms & Condition</p>
-    <div  style={{ "fontSize":"25px"   }} >
-    <ul><li>The Travel Expence of the participants whose colleges are <code className='text-warning'> Kanpur </code> reimbur outsideesd at the <code className='text-warning'> End of the Hackathon.</code></li>
-    <li>Please note that the total cost of <code className='text-warning'> Sleeper Seats </code> for <code className='text-warning'> Train </code> from your college to kanpur and back will be provided.</li>
-   <li>The procedure for providing travel Expence reimbursement will be communicated via  <code className='text-warning'> Email </code> or<code className='text-warning'> Discord. </code></li>
-   </ul>
-   </div>
+    <section id="travels">
+      <div className='col-12 col-md-12 d-flex flex-column align-items-center jusitfy-content-center' >
 
-
-
- 
-    
-  </div>
-  )
+        <h1 >Free Travel Expence</h1>
+        <p className='travels-terms'> <code className='text-warning'> Terms & Condition </code> </p>
+        <div style={{ "fontSize": "25px" }} >
+          <ul className='travels-list'><li>The Travel Expence of the participants whose colleges are <code className='text-warning'> Kanpur </code> reimbur outsideesd at the <code className='text-warning'> End of the Hackathon.</code></li>
+            <li>Please note that the total cost of <code className='text-warning'> Sleeper Seats </code> for <code className='text-warning'> Train </code> from your college to kanpur and back will be provided.</li>
+            <li>The procedure for providing travel Expence reimbursement will be communicated via  <code className='text-warning'> Email </code> or<code className='text-warning'> Discord. </code></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Travel
