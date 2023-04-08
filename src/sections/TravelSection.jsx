3e8796641d28a0da827122c41cd659fc94f7dd27
Mukul dubey <mdubey.0433@gmail.com>
2023-04-09 01:16:46 +0530

    basic typo & write travels to travel in css & jsx

--- src/sections/TravelSection.jsx
+++ src/sections/TravelSection.jsx
@@ -3,27 +3,17 @@ import React from 'react';
 const Travel = () => {
 
   return (
-    // <div>
-    // <h1>Free Travel Expence  </h1>
-    // <div style={{ "text-align": "center"}}>
-    // <p style={{ "fontSize":"50px" }}>Terms & Condition</p>
-    // <div style={{ "fontSize":"25px" }}>
-    // <li>The Travel Expence of the participants whose colleges are outside Kanpur reimburesd at the end of the Hackathon.</li>
-    // <li>Please note that the total cost of sleepr seat for train from your college to kanpur and back will be provided.</li>
-    // <li>The procedure for providing travel Expence reimbursement will be communicated via email or Discord.</li>
-    // </div>
-    // </div>
-    // </div>
-    <section id="travels">
-      <div className='col-12 col-md-12 d-flex flex-column align-items-center jusitfy-content-center' >
-
-        <h1 >Free Travel Expence</h1>
-        <p className='travels-terms'> <code className='text-warning'> Terms & Condition </code> </p>
-        <div style={{ "fontSize": "25px" }} >
-          <ul className='travels-list'><li>The Travel Expence of the participants whose colleges are <code className='text-warning'> Kanpur </code> reimbur outsideesd at the <code className='text-warning'> End of the Hackathon.</code></li>
-            <li>Please note that the total cost of <code className='text-warning'> Sleeper Seats </code> for <code className='text-warning'> Train </code> from your college to kanpur and back will be provided.</li>
-            <li>The procedure for providing travel Expence reimbursement will be communicated via  <code className='text-warning'> Email </code> or<code className='text-warning'> Discord. </code></li>
-          </ul>
+    <section id="travel">
+      <div className='d-flex flex-column align-items-center justify-content-between'>
+        <h1>Free Travel Expence</h1>
+        <div className='mt-5'>
+          <p className='travel-terms'> <span className='text-warning'> Terms & Condition </span> </p>
+          <div style={{ "fontSize": "25px" }} >
+            <ul className='travel-list'><li>The travel expenses of participants whose colleges are <span className='text-warning'> outside Kanpur </span> will be reimbursed at the<span className='text-warning'> End of the Hackathon.</span></li>
+              <li>Please note that the total cost of <span className='text-warning'> sleeper seats </span> for <span className='text-warning'> train travel </span> from your college to kanpur and back will be provided.</li>
+              <li>The procedure for providing travel expense reimbursement will be communicated via <span className='text-warning'> Email </span> or<span className='text-warning'> Discord. </span></li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
@@ -31,3 +21,4 @@ const Travel = () => {
 }
 
 export default Travel
+// col-12 col-md-12
