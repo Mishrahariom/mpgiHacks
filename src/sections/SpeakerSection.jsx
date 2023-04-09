b9e75c96a3a77a0a7d4318e37ba9f9a294ff91ba
Mukul dubey <mdubey.0433@gmail.com>
2023-04-09 23:53:47 +0530

    heightOfSpeakerSection

--- src/sections/SpeakerSection.jsx
+++ src/sections/SpeakerSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SpeakerSection = () => {
   return (
-    <div id="speakers" className='container-fluid vh-100' style={{"paddingTop": "7rem"}}>
+    <div id="speakers" className='container-fluid' style={{"paddingTop": "7rem"}}>
         <h1 className='text-center'>Speakers</h1>
-        <div className='card text-center bg-white' style={{"width": "18rem", "height": "25rem", "margin": "auto", "borderRadius": "2rem", "border": "1px solid white"}}>
+        <div className='card text-center bg-white' style={{"width": "18rem", "height": "25rem", "margin": "5rem auto", "borderRadius": "2rem", "border": "1px solid white"}}>
             <img src="https://i.ibb.co/X2BpSb6/Omkar-N-Srivastava.jpg" style={{"margin": "2rem auto 1rem", "width": "175px", "borderRadius": "50%"}} />
             <div className='card-body bg-black' style={{"borderRadius": "0 0 2rem 2rem"}}>
                 <p className='speakers-name'> Omkar N Srivastava</p>
